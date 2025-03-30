42b64b07e8f359a9764540cb7e4f7f9ffa821eb1
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-30 13:38:44 -0400

    Display products in the DOM with name, price, and image

--- cc_16.js
+++ cc_16.js
@@ -36,3 +36,39 @@ async function fetchProductsAsync(){
         handleError(error); // Error function will take care of any errors present
     }
 }
+
+// Task 4: Display the Products
+
+function displayProducts(products) {
+   const container = document.getElementById('product-container') // Selecting the product container
+   
+   const firstFiveProducts = products.slice(0,5); // Getting the first five products
+
+   firstFiveProducts.forEach(product => { // Loop through the first five products
+   
+    const productCards = document.createElement('div'); // Creating the product Cards
+    productCards.setAttribute('class','product-card')
+
+    const productName = document.createElement('h3'); // Creating Header element for product name
+    productName.setAttribute('class', 'product-header'); // Setting attributes for product name
+    productName.textContent = product.fields.name; // Setting text content to the product name
+
+    const productPrice = document.createElement('p'); // Creating Paragraph element for product price
+    productPrice.setAttribute('class', 'product-price' ); // Setting attributes for product price
+    productPrice.textContent = '$' + product.fields.price; // Setting text content to the product price
+
+    const productImage = document.createElement('img'); // Creating Img element for the product image
+    productImage.setAttribute('class', 'product-image'); // Setting attributes for product Image
+    productImage.src = product.fields.image[0].thumbnails.small.url; // Setting the product image URL
+    productImage.width = 300; // Setting the Width of the product image
+    productImage.height = 300; // Setting the height of the product image
+
+    productCards.appendChild(productName); // Appending product name to product cards div
+    productCards.appendChild(productPrice); // Appending product price to product cards div
+    productCards.appendChild(productImage); // Appending product image to product cards div
+
+    container.appendChild(productCards); // Appending product cards to the product container
+   }
+   )
+}
+
